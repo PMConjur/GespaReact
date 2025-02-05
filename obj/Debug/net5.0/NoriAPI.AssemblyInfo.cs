c1f198d160fcb0bc81d8773a1c7657da608996ee
Yoshua Rodriguez Cabrera <yoshua.rodriguez@conjur.mx>
2025-02-05 12:02:30 -0600

    Bearer y .gitignore test

--- obj/Debug/net5.0/NoriAPI.AssemblyInfo.cs
+++ obj/Debug/net5.0/NoriAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoriAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+add7a36302c668e198fa1a7166679b5aae1ace35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac9c9819096b45a5a7f8bc085ac26285f3311a83")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoriAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoriAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
