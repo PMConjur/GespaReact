961d58c200f388f2dfbd8159ddbdab2981d31753
martz25 <urielmartinez95@gmail.com>
2025-02-15 11:11:34 -0600

    resultados de busquedas por coincidencias

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,177 +23,175 @@ function OffcanvasExample() {
   const [suggestions, setSuggestions] = useState([]);
   const [showSuggestions, setShowSuggestions] = useState(false);
 
-  const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJCR1JIIiwianRpIjoiMTJmNTFmNTYtNDI5Yi00MzZlLWJlM2UtYzBhNjc0MWE2YmNjIiwiVXN1YXJpbyI6IkJHUkgiLCJleHAiOjE3Mzk1ODE2NjIsImlzcyI6IjE5Mi4xNjguNy4zMyIsImF1ZCI6IjE5Mi4xNjguNS4zOCJ9.yY1hnbY6NeFDXFO3BwmC9rOwg5CLQn90C_KvUvmEqMo'; // Reemplaza 'YOUR_ACCESS_TOKEN_HERE' con tu token de acceso 
+  const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJCR1JIIiwianRpIjoiODJhODQ2NzYtODE2YS00YzQ2LTgyZmUtMjZkM2FkZGM2MGQzIiwiVXN1YXJpbyI6IkJHUkgiLCJleHAiOjE3Mzk2NDEzNDgsImlzcyI6IjE5Mi4xNjguNy4zMyIsImF1ZCI6IjE5Mi4xNjguNS4zOCJ9.pESN3-1QvCLs-Wg7YqDMUXqINRQKhqAxOFv9v-AYqxI'; // Reemplaza 'YOUR_ACCESS_TOKEN_HERE' con tu token de acceso
 
-  useEffect(() => {
-    if (filter && !searchTerm) {
-      fetchFilterData(filter);
-    }
-  }, [filter]);
+ // Reemplaza 'YOUR_ACCESS_TOKEN_HERE' con tu token de acceso
 
-  const fetchFilterData = async (filter) => {
+ useEffect(() => {
+  if (filter && !searchTerm) {
+    fetchFilterData(filter);
+  }
+}, [filter]);
+
+const fetchFilterData = async (filter) => {
+  try {
+    const response = await axios.get('http://192.168.7.33/api/search-customer/busqueda-cuenta', {
+      params: { filtro: filter },
+      headers: {
+        Authorization: token,
+      },
+    });
+    console.log('Response for filter:', response.data);
+    setSearchResults(response.data.listaResultados || []);
+  } catch (error) {
+    console.error('Error fetching filter data:', error);
+  }
+};
+
+const handleSearch = async () => {
+  try {
+    const response = await axios.get('http://192.168.7.33/api/search-customer/busqueda-cuenta', {
+      params: { filtro: filter, ValorBusqueda: searchTerm },
+      headers: {
+        Authorization: token,
+      },
+    });
+    console.log('Response for search term:', response.data);
+    setSearchResults(response.data.listaResultados || []);
+  } catch (error) {
+    console.error('Error fetching search results:', error);
+  }
+};
+
+const handleFilterSelect = (filter) => {
+  setFilter(filter);
+  setSearchTerm('');
+};
+
+const handleInputChange = async (e) => {
+  const value = e.target.value;
+  setSearchTerm(value);
+
+  if (value.length > 0) {
     try {
-      const response = await axios.get('http://192.168.7.33/api/Search/busqueda-cuenta', {
-        params: { filtro: filter },
+      const response = await axios.get('http://192.168.7.33/api/search-customer/busqueda-cuenta', {
+        params: { filtro: filter, ValorBusqueda: value },
         headers: {
           Authorization: token,
         },
       });
-      console.log('Response for filter:', response.data);
-      setSearchResults(response.data.busquedainfo || []);
+      console.log('Response for suggestions:', response.data.listaResultados);
+      setSuggestions(response.data.listaResultados || []);
+      setShowSuggestions(true);
     } catch (error) {
-      console.error('Error fetching filter data:', error);
-    }
-  };
-
-  const handleSearch = async () => {
-    try {
-      const response = await axios.get('http://192.168.7.33/api/Search/busqueda-cuenta', {
-        params: { filtro: filter, ValorBusqueda: searchTerm },
-        headers: {
-          Authorization: token,
-        },
-      });
-      console.log('Response for search term:', response.data);
-      setSearchResults(Array.isArray(response.data.busquedainfo) ? response.data.busquedainfo : []);
-    } catch (error) {
-      console.error('Error fetching search results:', error);
-    }
-  };
-
-  const handleFilterSelect = (filter) => {
-    setFilter(filter);
-    setSearchTerm('');
-  };
-
-  const handleInputChange = async (e) => {
-    const value = e.target.value;
-    setSearchTerm(value);
-
-    if (value.length > 0) {
-      try {
-        const response = await axios.get('http://192.168.7.33/api/Search/busqueda-cuenta', {
-          params: { filtro: filter, ValorBusqueda: value },
-          headers: {
-            Authorization: token,
-          },
-        });
-        console.log('Response for suggestions:', response.data.busquedainfo);
-        if (Array.isArray(response.data.busquedainfo)) {
-          setSuggestions(response.data.busquedainfo);
-          setShowSuggestions(true);
-        } else {
-          setSuggestions([]);
-          setShowSuggestions(false);
-        }
-      } catch (error) {
-        console.error('Error fetching suggestions:', error);
-        setShowSuggestions(false);
-      }
-    } else {
-      setSuggestions([]);
+      console.error('Error fetching suggestions:', error);
       setShowSuggestions(false);
     }
-  };
-
-  const handleSuggestionClick = (suggestion) => {
-    setSearchTerm(suggestion.nombreDeudor);
+  } else {
     setSuggestions([]);
     setShowSuggestions(false);
-  };
+  }
+};
 
-  return (
-    <>
-      {[false].map((expand) => (
-        <Navbar
-          key={expand}
-          expand={expand}
-          className="bg-dark mb-3"
-          data-bs-theme="dark"
-          style={{
-            width: '100%',
-            height: '60px',
-            top: '0',
-            position: 'fixed',
-            left: '0',
-            zIndex: '1000',
-          }}
-        >
-          <Container fluid className="justify-content-between">
-            <Navbar.Toggle
-              aria-controls={`offcanvasNavbar-expand-${expand}`}
-              style={{ margin: '0 10px' }}
-            />
-            <Navbar.Brand
-              href="/home"
-              style={{ left: '0', marginRight: 'auto' }}
-              className="d-none d-md-block"
-            >
-              Gespa <Image src={Logo} style={{ width: '36px' }} roundedCircle />
-            </Navbar.Brand>
+const handleSuggestionClick = (suggestion) => {
+  setSearchTerm(suggestion.nombreDeudor);
+  setSuggestions([]);
+  setShowSuggestions(false);
+};
 
-            <form
-              className="d-flex"
+return (
+  <>
+    {[false].map((expand) => (
+      <Navbar
+        key={expand}
+        expand={expand}
+        className="bg-dark mb-3"
+        data-bs-theme="dark"
+        style={{
+          width: '100%',
+          height: '60px',
+          top: '0',
+          position: 'fixed',
+          left: '0',
+          zIndex: '1000',
+        }}
+      >
+        <Container fluid className="justify-content-between">
+          <Navbar.Toggle
+            aria-controls={`offcanvasNavbar-expand-${expand}`}
+            style={{ margin: '0 10px' }}
+          />
+          <Navbar.Brand
+            href="/home"
+            style={{ left: '0', marginRight: 'auto' }}
+            className="d-none d-md-block"
+          >
+            Gespa <Image src={Logo} style={{ width: '36px' }} roundedCircle />
+          </Navbar.Brand>
+
+          <form
+            className="d-flex"
+            data-bs-theme="dark"
+            style={{
+              gap: '10px',
+              alignContent: 'center',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+            }}
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSearch();
+            }}
+          >
+            <NavDropdown
+              title={`Filtrar por: ${filter}`}
+              id={`offcanvasNavbarDropdown-expand-${expand}`}
               data-bs-theme="dark"
-              style={{
-                gap: '10px',
-                alignContent: 'center',
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginLeft: 'auto',
-                marginRight: 'auto',
-              }}
-              onSubmit={(e) => {
-                e.preventDefault();
-                handleSearch();
-              }}
+              style={{ color: 'white' }}
+              className="d-none d-md-block"
+              onSelect={handleFilterSelect}
             >
-              <div style={{ position: 'relative', width: '300px' }}>
-                <FormControl
-                  type="search"
-                  placeholder="Buscar"
-                  className="me-2"
-                  aria-label="Search"
-                  value={searchTerm}
-                  onChange={handleInputChange}
-                  onFocus={() => setShowSuggestions(true)}
-                  onBlur={() => setTimeout(() => setShowSuggestions(false), 100)} // delay to allow click event
-                  style={{ width: '100%' }}
-                />
-                {showSuggestions && (
-                  <div style={{ position: 'absolute', top: '100%', left: '0', width: '100%', backgroundColor: 'white', border: '1px solid #ddd', zIndex: 1000 }}>
-                    {suggestions.map((suggestion, index) => (
-                      <div
-                        key={index}
-                        onClick={() => handleSuggestionClick(suggestion)}
-                        style={{ padding: '10px', cursor: 'pointer' }}
-                      >
-                        {suggestion.nombreDeudor}
-                      </div>
-                    ))}
-                  </div>
-                )}
-              </div>
-              <NavDropdown
-                title={`Filtrar por: ${filter}`}
-                id={`offcanvasNavbarDropdown-expand-${expand}`}
-                data-bs-theme="dark"
-                style={{ color: 'white' }}
-                className="d-none d-md-block"
-                onSelect={handleFilterSelect}
-              >
-                <NavDropdown.Item eventKey="Cuenta">Cuenta</NavDropdown.Item>
-                <NavDropdown.Item eventKey="Nombre">Nombre</NavDropdown.Item>
-                <NavDropdown.Item eventKey="RFC">RFC</NavDropdown.Item>
-                <NavDropdown.Item eventKey="Numero de cliente">Numero de cliente</NavDropdown.Item>
-                <NavDropdown.Item eventKey="Telefono">Telefono</NavDropdown.Item>
-                <NavDropdown.Item eventKey="Expediente">Expediente</NavDropdown.Item>
-              </NavDropdown>
-              <Button className="d-none d-md-block" variant="primary" type="submit">
-                Buscar
-              </Button>
-            </form>
+              <NavDropdown.Item eventKey="Cuenta">Cuenta</NavDropdown.Item>
+              <NavDropdown.Item eventKey="Nombre">Nombre</NavDropdown.Item>
+              <NavDropdown.Item eventKey="RFC">RFC</NavDropdown.Item>
+              <NavDropdown.Item eventKey="Numero de cliente">Numero de cliente</NavDropdown.Item>
+              <NavDropdown.Item eventKey="Telefono">Telefono</NavDropdown.Item>
+              <NavDropdown.Item eventKey="Expediente">Expediente</NavDropdown.Item>
+            </NavDropdown>
 
+            <div style={{ position: 'relative', width: '300px' }}>
+              <FormControl
+                type="search"
+                placeholder="Buscar"
+                className="me-2"
+                aria-label="Search"
+                value={searchTerm}
+                onChange={handleInputChange}
+                onFocus={() => setShowSuggestions(true)}
+                onBlur={() => setTimeout(() => setShowSuggestions(false), 200)} // delay to allow click event
+                style={{ width: '100%' }}
+              />
+              {showSuggestions && suggestions.length > 0 && (
+                <div style={{ position: 'absolute', top: '100%', left: '0', width: '100%', backgroundColor: 'white', color: 'black', border: '1px solid #ddd', maxHeight: '300px', overflowY: 'auto', zIndex: 1000 }}>
+                  {suggestions.map((suggestion, index) => (
+                    <div
+                      key={index}
+                      onClick={() => handleSuggestionClick(suggestion)}
+                      style={{ padding: '10px', cursor: 'pointer' }}
+                    >
+                      {suggestion.nombreDeudor}
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+
+            <Button className="d-none d-md-block" variant="primary" type="submit">
+              Buscar
+            </Button>
+          </form>
             <Dropdown
               as={ButtonGroup}
               drop="start"
@@ -255,7 +253,7 @@ function OffcanvasExample() {
                   </div>
                 </Nav>
               </Offcanvas.Body>
-            </Navbar.Offcanvas>
+              </Navbar.Offcanvas>
           </Container>
         </Navbar>
       ))}
@@ -269,4 +267,4 @@ function OffcanvasExample() {
   );
 }
 
-export default OffcanvasExample;
+export default OffcanvasExample
