2758dde3fcd3cc23a257fb8fc899298f61d856c4
YoshiRodriguez <munikaeru@gmail.com>
2025-02-19 19:11:03 -0600

    endpoint y Registro de Despausar

--- Services/EjecutivoService.cs
+++ Services/EjecutivoService.cs
@@ -10,32 +10,34 @@ namespace NoriAPI.Services
 {
     public interface IEjecutivoService
     {
-        
-        Task<TiemposEjecutivo> ValidateTimes(int NumEmpleado);
+
+        Task<TiemposEjecutivo> ValidateTimes(int numEmpleado);
+        Task<string> PauseUnpause(InfoPausa pausa);
 
     }
 
-    
+
 
     public class EjecutivoService : IEjecutivoService
     {
         private readonly IConfiguration _configuration;
         private readonly IEjecutivoRepository _ejecutivoRepository;
 
-        public EjecutivoService (IEjecutivoRepository ejecutivoRepository)
+        public EjecutivoService(IEjecutivoRepository ejecutivoRepository)
         {
             _ejecutivoRepository = ejecutivoRepository;
+
         }
 
-        
 
-        public async Task<TiemposEjecutivo> ValidateTimes(int NumEmpeado)
+
+        public async Task<TiemposEjecutivo> ValidateTimes(int numEmpleado)
         {
             ResultadoTiempos tiempos = null;
 
             try
             {
-                var validateTimes = await _ejecutivoRepository.ValidateTimes(NumEmpeado);
+                var validateTimes = await _ejecutivoRepository.ValidateTimes(numEmpleado);
                 tiempos = validateTimes;
 
             }
@@ -53,6 +55,53 @@ namespace NoriAPI.Services
 
 
         }
+
+        public async Task<string> PauseUnpause(InfoPausa pausa)
+        {
+            try
+            {
+                // Intenta despausar al ejecutivo validando su contraseña.
+                // Si la contraseña es incorrecta, retorna un mensaje de error.
+                if (!await Despausar(pausa))
+                {
+                    return "Contraseña Incorrecta.";
+                }
+
+                // Cambia el modo del ejecutivo a "Consulta".
+                await _ejecutivoRepository.ChangeEjecutivoMode(pausa.IdEjecutivo, "Consulta");
+
+                // Registra la pausa del ejecutivo en la base de datos con el idCatálogo1 3001 y la duración especificada.
+                // TODO: Asignar propiamente los valores del idCatalogo según el valor de PeCausa.
+                await _ejecutivoRepository.Pausa210(pausa.IdEjecutivo, 3001, pausa.Duracion);
+
+                // Aumenta el tiempo de actividad del ejecutivo con la duración de la pausa y la causa asociada.
+                await _ejecutivoRepository.IncreaseEjecutivoTime(pausa.IdEjecutivo, pausa.Duracion, pausa.PeCausa);
+            }
+            catch
+            {
+                // Si ocurre un error en cualquier parte del proceso, devuelve un mensaje de error.
+                return "Ocurrió un error al reanudar la sesión.";
+            }
+
+            // Retorna una cadena vacía si todo el proceso se ejecutó correctamente.
+            return "";
+        }
+
+        private async Task<bool> Despausar(InfoPausa tiempos)
+        {
+            // Valida la contraseña del ejecutivo en la base de datos.
+            var validatePass = await _ejecutivoRepository.ValidatePasswordEjecutivo(tiempos.IdEjecutivo, tiempos.Contrasenia);
+
+            // Si la validación falla (es nula), retorna false indicando que la contraseña es incorrecta.
+            if (validatePass is null)
+            {
+                return false;
+            }
+
+            // Si la validación es exitosa, retorna true.
+            return true;
+        }
+
 
     }
 }
