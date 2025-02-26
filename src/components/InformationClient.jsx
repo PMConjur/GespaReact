5650840aa7841202387c5eca0a05ac42fd207c8f
martz25 <urielmartinez95@gmail.com>
2025-02-26 12:49:18 -0600

    consumo enpoint informacion

--- src/components/InformationClient.jsx
+++ src/components/InformationClient.jsx
@@ -1,60 +1,93 @@
 import { Card, Table } from "react-bootstrap";
+import { fetchInformation } from "../services/gespawebServices"; // Importa el servicio
+import { useContext, useState, useEffect } from "react";
+import { AppContext } from "../pages/Managment"; // Importa el contexto DEL PADRE 
+import { toast, Toaster } from "sonner";
+
 const InformationClient = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false); 
+  const { searchResults } = useContext(AppContext);
+  const [toastShown, setToastShown] = useState(false); 
+
+  // Cargar datos desde la API al montar el componente
+  useEffect(() => {
+    const loadData = async () => {
+      setIsLoading(true);
+      try {
+        const information = await Promise.all(
+          searchResults.map(async (result, index) => {
+            const information  = await fetchInformation(result.idCuenta); // Usar result.idCuenta
+            return information ;
+          })
+        );
+        const flatInformation = information.flat();
+        setData(flatInformation); // Actualizar el estado con los datos de la API
+        if (flatInformation.length === 0 && !toastShown) {
+          toast.error("No hay carga de informacion", {
+            position: "top-right" // Mostrar toast en el lado derecho
+          });
+          setToastShown(true); // Marcar que el toast ya se mostró
+        }
+      } catch (error) {
+        console.error("Error al cargar los teléfonos:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loadData();
+  }, [searchResults, toastShown]); 
+
   return (
     <Card className="overflow-auto bg-dark">
       <Card.Body>
         <h5 className="card-title text-white">Información</h5>
         <Table hover className="table table-borderless" variant="dark">
           <tbody>
-            {[
-              [
-                "Empresa:",
-                "Gotica;Gotica",
-                "CBO / ING:",
-                "780",
-                "Fecha de Corte:",
-                "16/02/2025",
-                "Fecha de Asignación",
-                "24/10/2017"
-              ],
-              [
-                "Fecha de Cancelación:",
-                "",
-                "Llave 2:",
-                "Low Return Primes 45",
-                "Aniversario:",
-                "01/09/1986",
-                "Tipo de Crédito:",
-                "Lending OP"
-              ],
-              [
-                "Manual:",
-                "ACSI &",
-                "Saldo Inicial:",
-                "",
-                "INV / Atraso:",
-                "[Diasatraso] Días",
-                "Ejecutivo:",
-                "E-ANT"
-              ],
-              [
-                "Interés:",
-                "$",
-                "Fecha de Ultimo Pago:",
-                "",
-                "Monto de Ultimo Pago:",
-                "",
-                "Testeo/Contador:",
-                "2642"
-              ]
-            ].map((row, index) => (
-              <tr key={index}>
-                {row.map((cell, i) => (
-                  <th key={i} className={i % 2 !== 0 ? "text-success" : ""}>
-                    {cell}
-                  </th>
-                ))}
-              </tr>
+            {data.map((item, index) => (
+              <React.Fragment key={index}>
+                <tr>
+                  <th>Empresa:</th>
+                  <td className="text-success">{item['[employersname];[employersaddress]']}</td>
+                  <th>CBO / ING:</th>
+                  <td className="text-success">{item['[WO?] \\ [CBO] \\[BloqueoHerramienta] \\ [montlyincome]']}</td>
+                  <th>Fecha de Corte:</th>
+                  <td className="text-success">{item['[Fechacorte]']}</td>
+                  <th>Fecha de Asignación:</th>
+                  <td className="text-success">{item['[batchdate]']}</td>
+                </tr>
+                <tr>
+                  <th>Fecha de Cancelación:</th>
+                  <td className="text-success">{item['[CancellationDate]']}</td>
+                  <th>Llave 2:</th>
+                  <td className="text-success">{item['[CollectibilityCode] [SegmentoAMX] [DescuentoAMX]']}</td>
+                  <th>Aniversario:</th>
+                  <td className="text-success">{item['[anniversarydate] No Ofertar [BloqueoCorporate]']}</td>
+                  <th>Tipo de Crédito:</th>
+                  <td className="text-success">{item['[Product] [loan_productcode]']}</td>
+                </tr>
+                <tr>
+                  <th>Manual:</th>
+                  <td className="text-success">{item['[Valajuste] [AC MANUAL] & [Waiver]']}</td>
+                  <th>Saldo Inicial:</th>
+                  <td className="text-success">{item['[Balance G*]']}</td>
+                  <th>INV / Atraso:</th>
+                  <td className="text-success">{item['[INV] \\ [Diasatraso] Días']}</td>
+                  <th>Ejecutivo:</th>
+                  <td className="text-success">{item['[EJECUTIVO] ']}</td>
+                </tr>
+                <tr>
+                  <th>Interés:</th>
+                  <td className="text-success">{item['$']}</td>
+                  <th>Fecha de Ultimo Pago:</th>
+                  <td className="text-success">{item['[Last Date Payment]']}</td>
+                  <th>Monto de Ultimo Pago:</th>
+                  <td className="text-success">{item['[Amount]']}</td>
+                  <th>Testeo/Contador:</th>
+                  <td className="text-success">{item['[StayDaysOA]\\[SettlementAccountDays]\\[Conjurnet]\\[TESTAPR]']}</td>
+                </tr>
+              </React.Fragment>
             ))}
           </tbody>
         </Table>
