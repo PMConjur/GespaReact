006a07075a8dd9681e85eab9edeff023c52214d0
Uriel Saenz <projectmanagerconjur@gmail.com>
2025-02-25 19:46:37 -0600

    Correcion de Gestiones

--- src/components/Managments.jsx
+++ src/components/Managments.jsx
@@ -1,28 +1,30 @@
 import { Row, Col, Card } from "react-bootstrap";
 import Maintenance from "../assets/img/maintenance.png";
 import { auto } from "@popperjs/core";
+import Mantenimiento from "../assets/img/mantenimiento.jpg";
 const Managments = () => {
   return (
     <Row xs={auto} md={auto} className="g-2">
       {Array.from({ length: 1 }).map((_, idx) => (
-        <Col key={idx}>
-          <Card>
-            <Card.Img
-              variant="top"
-              src={Maintenance}
-              style={{
-                width: "250px",
-
-                objectFit: "cover",
-                resize: "both"
-              }}
-            />
-            <Card.Body>
-              <Card.Title>Gestiones</Card.Title>
-              <Card.Text>Estamos trabajando en ello...</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
+       <Col key={idx} md={12}>
+                 <Card>
+                             <div style={{ display: 'flex', justifyContent: 'center' }}> {/* Contenedor para centrar la imagen */}
+                               <Card.Img
+                                 variant="top"
+                                 src={Mantenimiento}
+                                 style={{
+                                   width: "300px",
+                                   objectFit: "cover",
+                                   resize: "both",
+                                 }}
+                               />
+                             </div>
+                             <Card.Body>
+                               <Card.Title>Gestiones</Card.Title>
+                               <Card.Text>Estamos trabajando en ello...</Card.Text>
+                             </Card.Body>
+                           </Card>
+               </Col>
       ))}
     </Row>
   );
--- src/pages/Managment.jsx
+++ src/pages/Managment.jsx
@@ -240,10 +240,9 @@ const Managment = () => {
                 </Row>
                 <Row>
                   <Col xs={12} md={8}>
-                    <h1>Prueba</h1>
+        
                   </Col>
                   <Col xs={12} md={4}>
-                    <h1>Recordatorios</h1>
                     <Reminder />
                   </Col>
                 </Row>
