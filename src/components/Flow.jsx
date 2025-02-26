7d04e7e590c938fcca8d1a3fe0a82a66b472e018
Uriel Saenz <projectmanagerconjur@gmail.com>
2025-02-25 19:44:50 -0600

    Correecion de flujo

--- src/components/Flow.jsx
+++ src/components/Flow.jsx
@@ -1,27 +1,29 @@
 import { auto } from "@popperjs/core";
 import { Row, Col, Card } from "react-bootstrap";
 import Maintenance from "../assets/img/maintenance.png";
+import Mantenimiento from "../assets/img/mantenimiento.jpg";
 const Flow = () => {
   return (
     <Row xs={auto} md={auto} className="g-2">
       {Array.from({ length: 1 }).map((_, idx) => (
-        <Col key={idx}>
+        <Col key={idx} md={12}>
           <Card>
-            <Card.Img
-              variant="top"
-              src={Maintenance}
-              style={{
-                width: "250px",
-
-                objectFit: "cover",
-                resize: "both"
-              }}
-            />
-            <Card.Body>
-              <Card.Title>Flujo</Card.Title>
-              <Card.Text>Estamos trabajando en ello...</Card.Text>
-            </Card.Body>
-          </Card>
+                      <div style={{ display: 'flex', justifyContent: 'center' }}> {/* Contenedor para centrar la imagen */}
+                        <Card.Img
+                          variant="top"
+                          src={Mantenimiento}
+                          style={{
+                            width: "300px",
+                            objectFit: "cover",
+                            resize: "both",
+                          }}
+                        />
+                      </div>
+                      <Card.Body>
+                        <Card.Title>Recordatorios</Card.Title>
+                        <Card.Text>Estamos trabajando en ello...</Card.Text>
+                      </Card.Body>
+                    </Card>
         </Col>
       ))}
     </Row>
