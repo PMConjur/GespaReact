3127b444e5d2ded4f613d08d3b72c303e9f6c03d
YoshiRodriguez <munikaeru@gmail.com>
2025-03-22 10:23:40 -0600

    Cambios de seguridad

--- Controllers/EjecutivoController.cs
+++ Controllers/EjecutivoController.cs
@@ -29,7 +29,7 @@ namespace NoriAPI.Controllers
 {
     [ApiController]
     [Route("api/ejecutivo")]
-    [Authorize]
+    //[Authorize]
     public class EjecutivoController : ControllerBase
     {
         private readonly IConfiguration _configuration;
