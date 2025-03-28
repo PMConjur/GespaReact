9ffb6cf333eac2ad58846292f6eadc731cfc9fcb
Omar <omar.ortiz@conjur.mx>
2025-03-28 13:36:53 -0600

    Diseño mejorado para situacion y Accionamiento implementado en lugar de Minimo mas atrasado

--- src/components/DataCard.jsx
+++ src/components/DataCard.jsx
@@ -1,80 +1,29 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Card, Row, Col } from "react-bootstrap";
-import { PersonFill, Cash, CurrencyDollar } from "react-bootstrap-icons";
+import { Card, Row, Col, Stack } from "react-bootstrap";
+import { PersonFill, Cash, ListOl } from "react-bootstrap-icons";
 import "../scss/styles.scss";
 import { useContext } from "react";
 import { AppContext } from "../pages/Managment";
-import CorazonRojo from "../assets/img/CRojo.jpg";
-import CorazonVerde from "../assets/img/CVerde.jpg";
-import CorazonBlanco from "../assets/img/CBlanco.jpg";
 
 const DataCard = () => {
   const { searchResults } = useContext(AppContext);
 
-  const renderSituacion = (situacion) => {
-    let iconoCorazon;
-    let colorTexto;
-
-    // Asignar la URL de la imagen y el color del texto según la situación
-    switch (situacion) {
-      case "Incumplió negociación":
-      case "En Proceso":
-      case "Accionamiento":
-        iconoCorazon = CorazonRojo; // Corazón rojo
-        colorTexto = "#f14b41"; // Texto rojo
-        break;
-      case "Sondeo":
-      case "Pagada":
-        iconoCorazon = CorazonVerde; // Corazón verde
-        colorTexto = "#65f3a3"; // Texto verde
-        break;
-      case "Nueva":
-      default:
-        iconoCorazon = CorazonBlanco; // Corazón blanco
-        colorTexto = "#ffffff"; // Texto blanco
-        break;
-    }
-
-    return (
-      <Card.Body>
-        <Card.Title>Situación:</Card.Title>
-        <div className="d-flex align-items-center">
-          <div className="card-icon rounded-circle d-flex align-items-center justify-content-center">
-            <img
-              src={iconoCorazon} // Renderizar la imagen como <img>
-              alt="Situación"
-              style={{
-                width: "42px",
-                height: "42px",
-                marginTop: "3px",
-              }}
-            />
-          </div>
-          <div className="ps-3">
-            <h6 style={{ fontSize: "1.8rem", color: colorTexto }}>
-              {situacion || "N/A"} {/* Mostrar "N/A" si no hay situación */}
-            </h6>
-            <span className="small pt-1 fw-bold">Negociación</span>
-          </div>
-        </div>
-      </Card.Body>
-    );
-  };
-
+  // Datos predeterminados en caso de que no haya resultados
   const defaultData = {
     nombreDeudor: "-",
     saldo: "-",
-    minimoAtrasado: "-",
-    situacion: "-",
+    minimoAtrasado: "-"
   };
 
+  // Usar el primer resultado o los datos predeterminados
   const result = searchResults[0] || defaultData;
 
+  // Formatear números con validación adicional
   const formatNumber = (number) => {
-    if (isNaN(number)) return "-";
+    if (number === null || number === undefined || isNaN(number)) return "-";
     return number.toLocaleString("es-MX", {
       minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
+      maximumFractionDigits: 2
     });
   };
 
@@ -123,32 +72,25 @@ const DataCard = () => {
         </Card>
       </Col>
 
-      <Col xxl={3} xl={6} md={6}>
+      <Col xxl={6} xl={6} md={6}>
         <Card className="warning-card text-light">
           <Card.Body>
-            <Card.Title>Mínimo Mas Atrasado:</Card.Title>
+            <Card.Title>Accionamiento:</Card.Title>
             <div className="d-flex align-items-center">
               <div className="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                <CurrencyDollar
-                  style={{ fontSize: "32px", color: "#f1a441" }}
-                />
+                <ListOl style={{ fontSize: "32px", color: "#D3BBF8" }} />
               </div>
               <div className="ps-3">
-                <h6 style={{ fontSize: "1.5rem", color: "#f1a441" }}>
-                  {result.minimoAtrasado !== "-"
-                    ? "$" + formatNumber(parseFloat(result.minimoAtrasado))
-                    : "-"}
-                </h6>
-                <span className="small pt-1 fw-bold">Monto reflejado</span>
+                <Stack direction="horizontal" gap={6}>
+                  <div className="p-2 action">SMS - 191</div>
+                  <div className="p-2 action">Carta - 1</div>
+                  <div className="p-2 action">Blaster - 33</div>
+                  <div className="p-2 action">Email - 25</div>
+                </Stack>
+                <span className="small pt-1 fw-bold">Conteo</span>
               </div>
             </div>
           </Card.Body>
-        </Card>
-      </Col>
-
-      <Col xxl={3} xl={6} md={6}>
-        <Card className="warning-card text-light">
-          {renderSituacion(result.situacion)}
         </Card>
       </Col>
     </Row>
--- src/components/SearchCustomer.jsx
+++ src/components/SearchCustomer.jsx
@@ -8,11 +8,70 @@ import {
   CalendarCheck,
   Clipboard
 } from "react-bootstrap-icons";
+import CorazonRojo from "../assets/img/CRojo.jpg";
+import CorazonVerde from "../assets/img/CVerde.jpg";
+import CorazonBlanco from "../assets/img/CBlanco.jpg";
 
 const SearchCustomer = () => {
   // Consume el contexto
   const { searchResults } = useContext(AppContext);
   const [isLoading, setIsLoading] = useState(true); // Estado para manejar la carga
+
+  const renderSituacion = (situacion) => {
+    let iconoCorazon;
+    let colorTexto;
+    let gradientText;
+
+    // Asignar la URL de la imagen y el color del texto según la situación
+    switch (situacion) {
+      case "Incumplió negociación":
+      case "En Proceso":
+      case "Accionamiento":
+        iconoCorazon = CorazonRojo; // Corazón rojo
+        colorTexto = "#4285F6"; // Texto rojo
+        gradientText = "header-red";
+        break;
+      case "Sondeo":
+      case "Pagada":
+        iconoCorazon = CorazonVerde; // Corazón verde
+        colorTexto = "#39fc8d"; // Texto verde
+        gradientText = "header-green";
+        break;
+      case "Nueva":
+      default:
+        iconoCorazon = CorazonBlanco; // Corazón blanco
+        colorTexto = "#f8f9fa"; // Texto blanco
+        gradientText = "header-white";
+        break;
+    }
+
+    return (
+      <Card.Body>
+        <div className="d-flex align-items-center">
+          <div className="card-icon-situation rounded-circle-situation d-flex align-items-center justify-content-center">
+            <img
+              src={iconoCorazon} // Renderizar la imagen como <img>
+              alt="Situación"
+              style={{
+                width: "90px",
+                height: "75px",
+                marginTop: "3px"
+              }}
+            />
+          </div>
+          <div className="ps-3 monochromatic-gradient-text">
+            <h6
+              style={{ fontSize: "1.8rem", color: colorTexto }}
+              className={gradientText}
+            >
+              {situacion || "N/A"} {/* Mostrar "N/A" si no hay situación */}
+            </h6>
+            <span className="small pt-1 fw-bold">Situación</span>
+          </div>
+        </div>
+      </Card.Body>
+    );
+  };
 
   // Datos por defecto en caso de que no haya resultados de búsqueda
   const defaultData = {
@@ -21,7 +80,8 @@ const SearchCustomer = () => {
     fechaActivacion: "--",
     expediente: "--",
     numeroCliente: "--",
-    rfc: "--"
+    rfc: "--",
+    situacion: "--"
   };
 
   // Obtener el primer resultado de búsqueda o usar los datos por defecto
@@ -29,73 +89,83 @@ const SearchCustomer = () => {
 
   useEffect(() => {
     setIsLoading(true); // Reinicia el estado a "cargando"
-    
+
     // Simula un tiempo de carga
     const timer = setTimeout(() => {
       setIsLoading(false);
     }, 500); // Tiempo de carga simulado
-    
+
     return () => clearTimeout(timer);
   }, [searchResults]); // Se ejecuta cada vez que searchResults cambia
 
   return (
-    <>
-      <br />
-      <Card className="mb-3 custom-card">
-        <Card.Body>
-          {isLoading ? (
-            // Mostrar placeholders mientras se cargan los datos
-            <Row style={{ color: "white" }}>
-              {[...Array(6)].map((_, i) => (
-                <Col key={i} md={4}>
-                  <Placeholder as="p" animation="glow">
-                    <Placeholder xs={6} />
-                  </Placeholder>
-                </Col>
-              ))}
-            </Row>
-          ) : (
-            // Mostrar los datos del cliente una vez cargados
-            <Row style={{ color: "white" }}>
-              {[
-                {
-                  icon: <CreditCard />,
-                  label: "Producto",
-                  value: "Amex"
-                },
-                {
-                  icon: <Clipboard />,
-                  label: "Cuenta",
-                  value: result.idCuenta || "--"
-                },
-                {
-                  icon: <CalendarCheck />,
-                  label: "Activada",
-                  value: result.fechaActivacion || "--"
-                },
-                {
-                  icon: <FileText />,
-                  label: "Expediente",
-                  value: `AMX${result.expediente}` || "--"
-                },
-                {
-                  icon: <Person />,
-                  label: "No. Cliente",
-                  value: result.numeroCliente || "--"
-                },
-                { icon: <FileText />, label: "RFC", value: result.rfc || "--" }
-              ].map(({ icon, label, value }, i) => (
-                <Col key={i} md={4}>
-                  <p>
-                    {icon} <strong>{label}:</strong> {value}
-                  </p>
-                </Col>
-              ))}
-            </Row>
-          )}
-        </Card.Body>
-      </Card>
-    </>
+    <Row>
+      <Col>
+        <Card className="mb-3 custom-card">
+          <Card.Body>
+            {isLoading ? (
+              // Mostrar placeholders mientras se cargan los datos
+              <Row style={{ color: "white" }}>
+                {[...Array(6)].map((_, i) => (
+                  <Col key={i} md={6}>
+                    <Placeholder as="p" animation="glow">
+                      <Placeholder xs={2} />
+                    </Placeholder>
+                  </Col>
+                ))}
+              </Row>
+            ) : (
+              // Mostrar los datos del cliente una vez cargados
+              <Row style={{ color: "white" }}>
+                {[
+                  {
+                    icon: <CreditCard />,
+                    label: "Producto",
+                    value: "Amex"
+                  },
+                  {
+                    icon: <Clipboard />,
+                    label: "Cuenta",
+                    value: result.idCuenta || "--"
+                  },
+                  {
+                    icon: <CalendarCheck />,
+                    label: "Activada",
+                    value: result.fechaActivacion || "--"
+                  },
+                  {
+                    icon: <FileText />,
+                    label: "Expediente",
+                    value: `AMX${result.expediente}` || "--"
+                  },
+                  {
+                    icon: <Person />,
+                    label: "No. Cliente",
+                    value: result.numeroCliente || "--"
+                  },
+                  {
+                    icon: <FileText />,
+                    label: "RFC",
+                    value: result.rfc || "--"
+                  }
+                ].map(({ icon, label, value }, i) => (
+                  <Col key={i} md={6}>
+                    <p>
+                      {icon} <strong>{label}:</strong> {value}
+                    </p>
+                  </Col>
+                ))}
+              </Row>
+            )}
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col xxl={4} xl={6} md={6} className="dashboard">
+        <Card className="situation-card text-light">
+          {renderSituacion(result.situacion)}
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
