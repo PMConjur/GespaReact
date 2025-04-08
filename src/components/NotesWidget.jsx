cf42fab8ca1722e6a176f51189c4741851f51341
fernando jaramillo <luis.jaramillo@conjur.com>
2025-04-08 17:42:43 -0600

    Actualización en NotesWidget

--- src/components/NotesWidget.jsx
+++ src/components/NotesWidget.jsx
@@ -244,6 +244,7 @@ function NotesWidget() {
     <div className="notes-widget card shadow">
       <div className="card-header bg-primary text-white d-flex justify-content-between align-items-center">
         <h5 className="mb-0">Mis Recordatorios</h5>
+        {/* 
         <button
           className="btn btn-sm btn-light"
           onClick={handleAddNote}
@@ -251,6 +252,7 @@ function NotesWidget() {
         >
           <PlusIcon />
         </button>
+        */}
       </div>
       <div className="card-body">
         {isEditing ? (
@@ -339,9 +341,11 @@ function NotesWidget() {
                 <p className="text-white">
                   No hay Recordatorio. ¡Crea uno nuevo!
                 </p>
+                {/* 
                 <button className="btn btn-primary" onClick={handleAddNote}>
-                  <PlusIcon /> <span className="ms-1">Nuevo Recordatorio</span>
-                </button>
+                <PlusIcon /> <span className="ms-1">Nuevo Recordatorio</span>
+                </button> 
+                */}
               </div>
             ) : (
               <div
