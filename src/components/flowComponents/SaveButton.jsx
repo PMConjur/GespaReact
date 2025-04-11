3e8cfff4ab1cc94eb13d9540b3128999d539b181
Omar <omar.ortiz@conjur.mx>
2025-04-11 10:11:06 -0600

    Mas correcciones para flujo cuando no contesto

--- src/components/flowComponents/SaveButton.jsx
+++ src/components/flowComponents/SaveButton.jsx
@@ -3,11 +3,22 @@ import { toast } from "sonner";
 import { useContext } from "react";
 import { AppContext } from "../../pages/Managment";
 
-const SaveButton = ({ onSave, isValid, data, onStopTimer, idContacto, valueContestaron }) => {
+const SaveButton = ({
+  onSave,
+  isValid,
+  data,
+  onStopTimer,
+  idContacto,
+  valueContestaron
+}) => {
   const { setTriggerUpdateStickyTime } = useContext(AppContext); // Obtiene la función para accionar la actualización del tiempo
 
   const handleSave = () => {
-    if (idContacto === 1109 || idContacto === 1129 || valueContestaron === "No") {
+    if (
+      idContacto === 1109 ||
+      idContacto === 1129 ||
+      valueContestaron === "No"
+    ) {
       setTriggerUpdateStickyTime((prev) => !prev); // Activa el trigger para actualizar el tiempo en StickyTimmer
       onStopTimer(); // Llama a la función para detener el cronómetro
       onSave(data); // Envía los datos al componente padre
